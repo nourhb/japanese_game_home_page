0254b4c1022e677cd523b1c5dbe1219b5f57186a
nourhb <nourhb>
2023-12-17 17:17:24 +0000

    Pushed from Locofy Builder

--- src/pages/Frame.js
+++ src/pages/Frame.js
@@ -1,124 +1,48 @@
-import "./Frame3.css";
+import "./Frame.css";
 
-const Frame3 = () => {
+const Frame = () => {
   return (
-    <div className="ravo-x-parent">
-      <img className="ravo-x-icon" alt="" src="/ravox@2x.png" />
-      <img className="frame-child77" alt="" src="/group-13872@2x.png" />
-      <img className="about-icon" alt="" src="/about@2x.png" />
-      <div className="frame-child78" />
-      <div className="sss-vip-parent">
-        <div className="sss-vip">SSS-VIP</div>
-        <div className="div50">$529</div>
-      </div>
-      <div className="polygon-parent">
-        <img className="polygon-icon" alt="" src="/polygon-2@2x.png" />
-        <div className="vector-group">
-          <img className="frame-child79" alt="" src="/rectangle-3@2x.png" />
-          <div className="s-vip">S-VIP</div>
-          <div className="div51">$129</div>
+    <div className="image-6-parent">
+      <img className="image-6-icon" alt="" src="/image-6@2x.png" />
+      <img className="image-4-icon" alt="" src="/image-4@2x.png" />
+      <img className="image-5-icon" alt="" src="/image-5@2x.png" />
+      <img className="valorant-icon" alt="" src="/valorant@2x.png" />
+      <img className="gradient-icon" alt="" src="/gradient@2x.png" />
+      <div className="group-parent1">
+        <img className="frame-child9" alt="" src="/group-13831@2x.png" />
+        <div className="group-parent2">
+          <img className="frame-child10" alt="" src="/group-13828@2x.png" />
+          <img className="frame-child10" alt="" src="/group-13808@2x.png" />
+          <img className="frame-child10" alt="" src="/group-13829@2x.png" />
+          <img className="frame-child10" alt="" src="/group-13830@2x.png" />
         </div>
       </div>
-      <div className="vector-container">
-        <img className="frame-child80" alt="" src="/rectangle-31@2x.png" />
-        <img className="image-1-icon2" alt="" src="/image-12@2x.png" />
-        <div className="early-access-parent">
-          <div className="early-access">Early Access</div>
-          <div className="check-in-1500-parent">
-            <div className="no-refund">Check in 15:00</div>
-            <div className="no-refund">No refund</div>
-          </div>
-        </div>
-        <div className="parent12">
-          <div className="sss-vip">$99</div>
-          <img
-            className="vuesaxlineararrow-right-icon1"
-            alt=""
-            src="/vuesaxlineararrowright@2x.png"
-          />
-        </div>
-        <img className="frame-child81" alt="" src="/polygon-1@2x.png" />
-      </div>
-      <b className="head">Ticket</b>
-      <img className="frame-child82" alt="" src="/group-16@2x.png" />
-      <img className="frame-child83" alt="" src="/group-9@2x.png" />
-      <div className="frame-parent14">
-        <div className="ellipse-container">
-          <div className="frame-child84" />
-          <img className="image-2-icon1" alt="" src="/image-21@2x.png" />
-        </div>
-        <img className="frame-child85" alt="" src="/group-11@2x.png" />
-        <img className="frame-child86" alt="" src="/group-12@2x.png" />
-      </div>
-      <div className="frame-parent15">
-        <div className="vuesaxlinearcalendar-parent">
-          <img
-            className="vuesaxlineararrow-right-icon1"
-            alt=""
-            src="/vuesaxlineardirectleft3@2x.png"
-          />
-          <div className="oct-12-">13:00 - 19:00, Oct 12 - 2023</div>
-        </div>
-        <div className="vuesaxlinearcalendar-parent">
-          <img
-            className="vuesaxlineararrow-right-icon1"
-            alt=""
-            src="/vuesaxlineardirectleft3@2x.png"
-          />
-          <div className="oct-12-">West Point St., Birmingham Dtr.</div>
-        </div>
-      </div>
-      <div className="info">
-        <div className="vuesaxlinearcalendar-parent">
-          <img
-            className="vuesaxlineararrow-right-icon1"
-            alt=""
-            src="/vuesaxlinearcalendar@2x.png"
-          />
-          <div className="oct-12-">13:00 - 19:00, Oct 12 - 2023</div>
-        </div>
-        <div className="vuesaxlinearcalendar-parent">
-          <img
-            className="vuesaxlineararrow-right-icon1"
-            alt=""
-            src="/vuesaxlinearmap@2x.png"
-          />
-          <div className="oct-12-">West Point St., Birmingham Dtr.</div>
-        </div>
-      </div>
-      <div className="body">{`Евгений Чебатков - казахстанский и российский стендап комик, актер и подкастер. Чебатков-это комик со своим комедийным голосом, оригинальными темами и узнаваемым стилем. Вы могли видеть его концерты `}</div>
-      <img className="frame-child87" alt="" src="/group-7@2x.png" />
-      <img className="frame-child88" alt="" src="/group-14@2x.png" />
-      <img className="rectangle-icon1" alt="" src="/rectangle@2x.png" />
-      <div className="social2">
-        <img className="social-child4" alt="" src="/group-111@2x.png" />
-        <img className="frame-child86" alt="" src="/group-121@2x.png" />
-      </div>
-      <img className="line-icon1" alt="" src="/line@2x.png" />
-      <img className="frame-child89" alt="" src="/group-13879@2x.png" />
-      <img className="vector-icon" alt="" src="/vector@2x.png" />
-      <img className="vector-icon1" alt="" src="/vector1@2x.png" />
-      <img className="vector-icon2" alt="" src="/vector2@2x.png" />
-      <img className="vector-icon3" alt="" src="/vector3@2x.png" />
-      <img className="frame-child90" alt="" src="/group-13873@2x.png" />
-      <img className="frame-child91" alt="" src="/group-13878@2x.png" />
-      <div className="discover-parent">
-        <div className="sss-vip">Discover</div>
+      <div className="noti1">
+        <div className="div3">3</div>
         <img
-          className="vuesaxlineararrow-right-icon1"
+          className="vuesaxlinearticket-icon2"
           alt=""
-          src="/vuesaxlineararrowright1@2x.png"
+          src="/vuesaxlinearticket1@2x.png"
         />
+        <div className="noti-child1" />
+        <div className="div3">65</div>
+        <img
+          className="vuesaxlinearticket-icon2"
+          alt=""
+          src="/vuesaxlinearwallet1@2x.png"
+        />
+        <div className="noti-child1" />
+        <img className="noti-child3" alt="" src="/group-13842@2x.png" />
       </div>
-      <div className="home-parent">
-        <div className="home">Home</div>
-        <div className="home">Guide book</div>
-        <div className="home">Settings</div>
+      <div className="social">
+        <img className="social-child" alt="" src="/group-7@2x.png" />
+        <img className="social-child" alt="" src="/group-11@2x.png" />
+        <img className="social-child" alt="" src="/group-12@2x.png" />
       </div>
-      <div className="inventory">Inventory</div>
-      <img className="frame-child92" alt="" src="/group-13881@2x.png" />
+      <img className="frame-child14" alt="" src="/group-13886@2x.png" />
+      <img className="union-icon" alt="" src="/union@2x.png" />
     </div>
   );
 };
 
-export default Frame3;
+export default Frame;
