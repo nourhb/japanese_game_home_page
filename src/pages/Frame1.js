46d8ed87229a144a7568c052fdd2f11a1dde57f8
nourhb <nourhb>
2023-12-17 17:20:35 +0000

    Pushed from Locofy Builder

---
+++ src/pages/Frame1.js
@@ -0,0 +1,47 @@
+import "./Frame1.css";
+
+const Frame1 = () => {
+  return (
+    <div className="valorant-parent">
+      <img className="valorant-icon1" alt="" src="/valorant1@2x.png" />
+      <img className="image-5-icon1" alt="" src="/image-51@2x.png" />
+      <img className="image-4-icon1" alt="" src="/image-41@2x.png" />
+      <img className="gradient-icon1" alt="" src="/gradient@2x.png" />
+      <div className="group-parent3">
+        <img className="frame-child15" alt="" src="/group-13831@2x.png" />
+        <div className="group-parent4">
+          <img className="frame-child16" alt="" src="/group-13828@2x.png" />
+          <img className="frame-child16" alt="" src="/group-13808@2x.png" />
+          <img className="frame-child16" alt="" src="/group-13829@2x.png" />
+          <img className="frame-child16" alt="" src="/group-13830@2x.png" />
+        </div>
+      </div>
+      <div className="noti2">
+        <div className="div5">3</div>
+        <img
+          className="vuesaxlinearticket-icon3"
+          alt=""
+          src="/vuesaxlinearticket1@2x.png"
+        />
+        <div className="noti-child4" />
+        <div className="div5">65</div>
+        <img
+          className="vuesaxlinearticket-icon3"
+          alt=""
+          src="/vuesaxlinearwallet1@2x.png"
+        />
+        <div className="noti-child4" />
+        <img className="noti-child6" alt="" src="/group-138421@2x.png" />
+      </div>
+      <div className="social1">
+        <img className="social-child1" alt="" src="/group-71@2x.png" />
+        <img className="social-child1" alt="" src="/group-11@2x.png" />
+        <img className="social-child1" alt="" src="/group-12@2x.png" />
+      </div>
+      <img className="frame-child20" alt="" src="/group-138861@2x.png" />
+      <img className="union-icon1" alt="" src="/union1@2x.png" />
+    </div>
+  );
+};
+
+export default Frame1;
