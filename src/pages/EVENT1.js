fd763dd91bb42831a003ddd23c0b7b20a15d1aab
nourhb <nourhb>
2023-12-17 17:12:46 +0000

    First Commit from Locofy Builder

---
+++ src/pages/EVENT1.js
@@ -0,0 +1,440 @@
+import "./EVENT1.css";
+
+const EVENT1 = () => {
+  return (
+    <div className="event">
+      <div className="frame-group">
+        <div className="frame-container">
+          <div className="frame-wrapper">
+            <div className="event-parent">
+              <div className="event1">Event</div>
+              <div className="parent">
+                <div className="busy">3</div>
+                <div className="passed">Passed</div>
+              </div>
+              <div className="group">
+                <div className="busy">29</div>
+                <div className="upcoming">Upcoming</div>
+              </div>
+            </div>
+          </div>
+          <div className="frame-wrapper1">
+            <div className="busy-parent">
+              <div className="busy">Busy</div>
+              <div className="container">
+                <div className="busy">14</div>
+                <div className="upcoming">hrs</div>
+              </div>
+            </div>
+          </div>
+          <div className="frame-parent1">
+            <div className="free-days-parent">
+              <div className="busy">Free days</div>
+              <div className="parent1">
+                <div className="busy">53</div>
+                <div className="upcoming">days</div>
+              </div>
+            </div>
+            <div className="repeatable-parent">
+              <div className="busy">Repeatable</div>
+              <div className="parent2">
+                <div className="busy">32</div>
+                <div className="upcoming">events</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <img className="img-icon1" alt="" src="/img@2x.png" />
+        <div className="schedule">
+          <div className="schedule-child" />
+          <div className="schedule-item" />
+          <div className="schedule-item" />
+          <div className="schedule-item" />
+          <div className="schedule-item" />
+          <div className="schedule-item" />
+          <div className="schedule-item" />
+          <div className="schedule-item" />
+        </div>
+        <div className="schedule-parent">
+          <div className="tue">Schedule</div>
+          <div className="div12">03</div>
+        </div>
+        <div className="tue-parent">
+          <div className="tue">Tue</div>
+          <div className="div13">02</div>
+        </div>
+        <div className="wed-parent">
+          <div className="tue">Wed</div>
+          <div className="div13">03</div>
+        </div>
+        <div className="thu-parent">
+          <div className="tue">Thu</div>
+          <div className="div13">04</div>
+        </div>
+        <div className="fri-parent">
+          <div className="tue">Fri</div>
+          <div className="div13">05</div>
+        </div>
+        <div className="sat-parent">
+          <div className="tue">Sat</div>
+          <div className="div13">06</div>
+        </div>
+        <div className="sun-parent">
+          <div className="tue">Sun</div>
+          <div className="div12">07</div>
+        </div>
+        <div className="noti3">
+          <div className="busy">3</div>
+          <img
+            className="vuesaxlinearsms-icon"
+            alt=""
+            src="/vuesaxlinearsms@2x.png"
+          />
+          <div className="noti-child7" />
+          <img className="noti-child8" alt="" src="/group-13842@2x.png" />
+        </div>
+        <div className="rectangle-container">
+          <div className="frame-child21" />
+          <div className="participant">Participant</div>
+          <div className="joins-4-rejects">
+            35 joins, 4 rejects, 108 pendings
+          </div>
+          <div className="rectangle-parent1">
+            <img
+              className="frame-child22"
+              alt=""
+              src="/rectangle-4371@2x.png"
+            />
+            <img
+              className="frame-child22"
+              alt=""
+              src="/rectangle-4372@2x.png"
+            />
+            <img
+              className="frame-child22"
+              alt=""
+              src="/rectangle-4373@2x.png"
+            />
+            <img
+              className="frame-child22"
+              alt=""
+              src="/rectangle-4374@2x.png"
+            />
+            <img
+              className="frame-child22"
+              alt=""
+              src="/rectangle-4375@2x.png"
+            />
+          </div>
+          <img
+            className="vuesaxlineardirect-left-icon4"
+            alt=""
+            src="/vuesaxlineardirectleft1@2x.png"
+          />
+        </div>
+        <div className="time">
+          <div className="upcoming-event">15:00</div>
+        </div>
+        <div className="back-parent">
+          <div className="back1">
+            <div className="vuesaxlineardirect-left-wrapper">
+              <img
+                className="vuesaxlineardirect-left-icon5"
+                alt=""
+                src="/vuesaxlineardirectleft5@2x.png"
+              />
+            </div>
+          </div>
+          <div className="upcoming-event-parent">
+            <div className="upcoming-event">Upcoming Event</div>
+            <div className="thu-1500">Thu, 15:00</div>
+          </div>
+        </div>
+        <div className="line-parent">
+          <div className="frame-child27" />
+          <div className="frame-child27" />
+          <div className="frame-child27" />
+          <div className="frame-child30" />
+          <div className="frame-child27" />
+          <div className="frame-child27" />
+          <div className="frame-child33" />
+        </div>
+        <div className="ellipse-parent">
+          <div className="ellipse-div" />
+          <img className="frame-child34" alt="" src="/rectangle-4379@2x.png" />
+          <div className="main-festival-concert">
+            Main Festival: Concert of...
+          </div>
+          <div className="div21">19:00</div>
+        </div>
+        <div className="rectangle-parent2">
+          <img className="frame-child35" alt="" src="/rectangle-4381@2x.png" />
+          <div className="div22">14:00</div>
+          <div className="honkai-impact">Honkai Impact ...</div>
+        </div>
+        <div className="frame-parent2">
+          <div className="parent3">
+            <div className="busy">16:00</div>
+            <div className="war-of-paxithl">War of Paxithl ...</div>
+          </div>
+          <img className="frame-child36" alt="" src="/rectangle-4380@2x.png" />
+        </div>
+        <img className="frame-child37" alt="" src="/rectangle-4382@2x.png" />
+        <div className="frame-child38" />
+        <div className="alert-parent">
+          <div className="busy">Alert</div>
+          <div className="hr-parent">
+            <div className="busy">1hr</div>
+            <div className="upcoming">before</div>
+          </div>
+        </div>
+        <div className="repeat-parent">
+          <div className="busy">Repeat</div>
+          <div className="yearly">Yearly</div>
+        </div>
+        <div className="wrapper-fuyu-matsuri">
+          <img
+            className="fuyu-matsuri-icon"
+            alt=""
+            src="/fuyu--matsuri@2x.png"
+          />
+        </div>
+        <div className="wrapper">
+          <div className="upcoming-event">18:00</div>
+        </div>
+        <img
+          className="vuesaxlinearsetting-icon"
+          alt=""
+          src="/vuesaxlinearsetting@2x.png"
+        />
+      </div>
+      <div className="calendar">
+        <div className="date-picker-element-calendar-parent">
+          <div className="date-picker-element-calendar">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="text">Su</div>
+          </div>
+          <div className="date-picker-element-calendar1">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="text">Mo</div>
+          </div>
+          <div className="date-picker-element-calendar2">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="text">Tu</div>
+          </div>
+          <div className="date-picker-element-calendar3">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="text">We</div>
+          </div>
+          <div className="date-picker-element-calendar4">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="text">Th</div>
+          </div>
+          <div className="date-picker-element-calendar5">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="text">Fr</div>
+          </div>
+          <div className="date-picker-element-calendar6">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="text">Sa</div>
+          </div>
+          <div className="date-picker-element-calendar7">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="label">77</div>
+          </div>
+          <div className="date-picker-element-calendar8">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="label">77</div>
+          </div>
+          <div className="date-picker-element-calendar9">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="text">1</div>
+          </div>
+          <div className="date-picker-element-calendar10">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="text">2</div>
+          </div>
+          <div className="date-picker-element-calendar11">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="text">3</div>
+          </div>
+          <div className="date-picker-element-calendar12">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="text">4</div>
+          </div>
+          <div className="date-picker-element-calendar13">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="text">5</div>
+          </div>
+          <div className="date-picker-element-calendar14">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="text">6</div>
+          </div>
+          <div className="date-picker-element-calendar15">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="text">7</div>
+          </div>
+          <div className="date-picker-element-calendar16">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="text">8</div>
+          </div>
+          <div className="date-picker-element-calendar17">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="text">9</div>
+          </div>
+          <div className="date-picker-element-calendar18">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="text">10</div>
+          </div>
+          <div className="date-picker-element-calendar19">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="text">11</div>
+          </div>
+          <div className="date-picker-element-calendar20">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="text">12</div>
+          </div>
+          <div className="date-picker-element-calendar21">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="text">13</div>
+          </div>
+          <div className="date-picker-element-calendar22">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="text">14</div>
+          </div>
+          <div className="date-picker-element-calendar23">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="text">15</div>
+          </div>
+          <div className="date-picker-element-calendar24">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="text">16</div>
+          </div>
+          <div className="date-picker-element-calendar25">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="text">17</div>
+          </div>
+          <div className="date-picker-element-calendar26">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="text">18</div>
+          </div>
+          <div className="date-picker-element-calendar27">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="text">19</div>
+          </div>
+          <div className="date-picker-element-calendar28">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="text">20</div>
+          </div>
+          <div className="date-picker-element-calendar29">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="text">21</div>
+          </div>
+          <div className="date-picker-element-calendar30">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="text">22</div>
+          </div>
+          <div className="date-picker-element-calendar31">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="text">23</div>
+          </div>
+          <div className="date-picker-element-calendar32">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="text">24</div>
+          </div>
+          <div className="date-picker-element-calendar33">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="text">25</div>
+          </div>
+          <div className="date-picker-element-calendar34">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="text">26</div>
+          </div>
+          <div className="date-picker-element-calendar35">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="text">27</div>
+          </div>
+          <div className="date-picker-element-calendar36">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="text">28</div>
+          </div>
+          <div className="date-picker-element-calendar37">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="text">29</div>
+          </div>
+          <div className="date-picker-element-calendar38">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="text">30</div>
+          </div>
+          <div className="date-picker-element-calendar39">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="text">31</div>
+          </div>
+          <div className="date-picker-element-calendar40">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="label">77</div>
+          </div>
+          <div className="date-picker-element-calendar41">
+            <div className="signed-by-base-date-picker-el" />
+            <div className="label">77</div>
+          </div>
+        </div>
+      </div>
+      <div className="side-menu">
+        <img className="image-1-icon" alt="" src="/image-1@2x.png" />
+        <div className="noti-parent">
+          <div className="noti4">
+            <div className="busy">Unread</div>
+            <div className="noti-child7" />
+            <div className="accept">Accept</div>
+          </div>
+          <div className="instance-parent">
+            <div className="group-div">
+              <div className="rectangle-parent3">
+                <div className="group-child1" />
+                <div className="group-child2" />
+                <div className="nov-22-20231">06:00, Nov 22, 2023</div>
+                <div className="repeat-none1">Repeat - None</div>
+                <img
+                  className="group-child3"
+                  alt=""
+                  src="/rectangle-43811@2x.png"
+                />
+                <div className="adidas-giveaway1">
+                  <p className="product">Adidas</p>
+                  <p className="product">Giveaway</p>
+                </div>
+              </div>
+              <div className="instance-child" />
+            </div>
+            <div className="group-div">
+              <div className="rectangle-parent3">
+                <div className="group-child1" />
+                <div className="group-child2" />
+                <div className="nov-22-20231">07:00, Dec 23, 2023</div>
+                <div className="repeat-none1">Repeat - None</div>
+                <img
+                  className="group-child3"
+                  alt=""
+                  src="/rectangle-43812@2x.png"
+                />
+                <div className="adidas-giveaway1">
+                  <p className="product">Product</p>
+                  <p className="product">Meeting</p>
+                </div>
+              </div>
+              <div className="instance-child" />
+            </div>
+          </div>
+        </div>
+        <div className="group-parent6">
+          <img className="frame-child39" alt="" src="/group-13828@2x.png" />
+          <img className="frame-child39" alt="" src="/group-13808@2x.png" />
+          <img className="frame-child39" alt="" src="/group-13829@2x.png" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EVENT1;
